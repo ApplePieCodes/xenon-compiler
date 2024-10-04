3cf1a59ce8e6cbd9f8c647dbd12a43b94196596a
ApplePieCodes <127224911+ApplePieCodes@users.noreply.github.com>
2024-10-03 20:22:01 -0400

    Update

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,3 @@ mod x86_64;
 fn main() {
     let mut args: Vec<String> = env::args().collect();
 }
-
-
-fn compile(program: Program) -> String {
-    x86_64::compile();
-}
--- src/x86_64.rs
+++ src/x86_64.rs
@@ -1,7 +1,49 @@
-pub fn compile() -> String {
+use std::borrow::{Borrow, BorrowMut};
 
+use xenon_parser::node::{Definition, Namespace, Program};
+
+
+struct SemanticParser {
+    program: Program,
+    variables: Vec<Variable>,
+    namespaces: Vec<String>
 }
 
-fn parse_function() -> String {
+impl SemanticParser {
+    pub fn new(program: Program) -> Self {
+        SemanticParser {
+            program,
+            variables: Vec::new(),
+            namespaces: Vec::new()
+        }
+    }
+
+    pub fn parse(&mut self) {
+        let namespaces = std::mem::take(&mut self.program.namespaces);
+        for namespace in namespaces {
+            self.parse_namespace(namespace);
+        }
+    }
     
+    
+
+    pub fn parse_namespace(&mut self, namespace: Namespace) {
+        if ! self.namespaces.contains(&namespace.name) {
+            self.namespaces.push(namespace.name);
+        }
+        let definitions = std::mem::take(&mut namespace.definitions);
+        for definition in definitions {
+            self.parse_definition(definition);
+        }
+    }
+
+    pub fn parse_definition(&mut self, &mut definition: Definition) {
+        
+    }
+}
+
+
+struct Variable {
+    pub name: String,
+    pub vtype: String
 }
